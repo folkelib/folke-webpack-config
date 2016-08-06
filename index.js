48904bbe08126cf9d19451cb5de3f4537deea768
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-08-06 20:10:20 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,28 @@
+﻿const extractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: 'wwwroot',
+        filename: 'index.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
+        modulesDirectories: ['node_modules'],
+        root: path.resolve("./node_modules")
+    },
+    module: {
+        loaders: [
+          { test: /\.ts$/, loader: 'ts-loader' },
+          { test: /\.html$/, loader: 'raw-loader' },
+          { test: /\.less$/, loader: extractTextPlugin.extract('style-loader', 'css-loader!less-loader') },
+          { test: /\.(jpe?g|gif|png|eot|svg|woff2?|ttf)(\?.*)?$/, loader: 'file-loader' }
+        ]
+    },
+    plugins: [
+        new extractTextPlugin('[name].css')
+    ],
+    devtool: 'source-map',
+}
