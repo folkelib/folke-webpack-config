b68d0ebc4f75d4594943450dd27a25acdf4a6f73
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-11-05 12:25:02 +0100

    Update to webpack 3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 ﻿"use strict";
-const extractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const fs = require('fs');
@@ -7,40 +7,44 @@ const fs = require('fs');
 const environment = process.env.ASPNETCORE_ENVIRONMENT || process.env.NODE_ENV;
 const production = environment && (environment.startsWith("prod") || environment == "staging");
 
-const plugins = [ new extractTextPlugin('[name].css') ];
+const extractTextPlugin = new ExtractTextPlugin('[name].css');
+const plugins = [ extractTextPlugin ];
 if (production) {
     plugins.push(new webpack.optimize.UglifyJsPlugin());
 }
 
-const files = fs.readdirSync("./src");
-const entries = {};
-for (let file of files) {
-    if (!file.match(/\.d\.ts$/)) {
-        const match = file.match(/^(.*)\.ts$/);
-        if (match) {
-            entries[match[1]] = "./src/" + file;
+module.exports = function(dir) {
+    const files = fs.readdirSync(path.join(dir, "./src"));
+    const entries = {};
+    for (let file of files) {
+        if (!file.match(/\.d\.ts$/)) {
+            const match = file.match(/^(.*)\.tsx?$/);
+            if (match) {
+                entries[match[1]] = "./src/" + file;
+            }
         }
     }
-}
 
-module.exports = {
-    entry: entries,
-    output: {
-        path: 'wwwroot',
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
-    },
-    module: {
-        loaders: [
-          { test: /\.ts$/, loader: 'ts-loader' },
-          { test: /\.html$/, loader: 'raw-loader' },
-          { test: /\.less$/, loader: extractTextPlugin.extract('style-loader', 'css-loader!less-loader') },
-          { test: /\.(jpe?g|gif|png|eot|svg|woff2?|ttf)(\?.*)?$/, loader: 'file-loader' }
-        ]
-    },
-    plugins: plugins,
-    devtool: 'source-map',
+    return {
+        entry: entries,
+        context: path.resolve(dir, ""),
+        output: {
+            path: path.resolve(dir, 'wwwroot'),
+            filename: '[name].js',
+            publicPath: '/'
+        },
+        resolve: {
+            extensions: ['.tsx', '.jsx', '.ts', '.js']
+        },
+        module: {
+            loaders: [
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.html$/, loader: 'raw-loader' },
+            { test: /\.less$/, loader: extractTextPlugin.extract('style-loader', 'css-loader!less-loader') },
+            { test: /\.(jpe?g|gif|png|eot|svg|woff2?|ttf)(\?.*)?$/, loader: 'file-loader' }
+            ]
+        },
+        plugins: plugins,
+        devtool: 'source-map',
+    }
 }
